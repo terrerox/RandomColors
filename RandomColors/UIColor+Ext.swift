e5cf9449833692c7931e8cbb434ced2ddb7622e9
terrerox <terrero0611@gmail.com>
2024-01-21 15:26:45 -0400

    init

--- RandomColors/UIColor+Ext.swift
+++ RandomColors/UIColor+Ext.swift
@@ -5,4 +5,14 @@
 //  Created by terrerox on 24/12/23.
 //
 
-import Foundation
+import UIKit
+
+extension UIColor {
+    static func random() -> UIColor {
+        let randomColor = UIColor(red: CGFloat.random(in: 0...1),
+                                  green: CGFloat.random(in: 0...1),
+                                  blue: CGFloat.random(in: 0...1),
+                                  alpha: 1)
+        return randomColor
+    }
+}
